578049d64aeb42946d66a4c1abf49ec015618020
Matheus Guida <159961710+Nevek20@users.noreply.github.com>
2025-01-28 08:59:42 -0300

    Add files via upload

---
+++ Sitema2/Sitema2/cadCliente.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sitema2
+{
+
+    public partial class cadCliente : Form
+    {
+
+
+        public cadCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            maskedTextBox1.Text = string.Empty;
+            maskedTextBox2.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            maskedTextBox3.Text = string.Empty;
+            maskedTextBox4.Text = string.Empty;
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label8.Text = "Cadastrado com sucesso!!!";
+            label8.ForeColor = Color.Green;
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+        private bool ValidarCpf(string cpf)
+        {
+            // Remove qualquer caractere não numérico
+            cpf = Regex.Replace(cpf, @"[^\d]", "");
+
+            // Verifica se tem 11 dígitos
+            if (cpf.Length != 11)
+                return false;
+
+            // Verifica se o CPF é uma sequência de números iguais (ex.: 111.111.111-11)
+            if (new string(cpf[0], 11) == cpf)
+                return false;
+
+            // Calculando o primeiro dígito verificador
+            int soma = 0;
+            int peso = 10;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * peso--;
+            }
+
+            int resto = soma % 11;
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+            if (digito1 != int.Parse(cpf[9].ToString()))
+                return false;
+
+            // Calculando o segundo dígito verificador
+            soma = 0;
+            peso = 11;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * peso--;
+            }
+
+            resto = soma % 11;
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+            return digito2 == int.Parse(cpf[10].ToString());
+        }
+    }
+}
