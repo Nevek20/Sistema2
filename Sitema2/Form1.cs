6f5acac0ada88d861308f086cd3fc756db8975c1
matheus.guida <matheus.guida@AME0556351W10-1>
2025-01-28 08:55:49 -0300

    pimba

---
+++ Sitema2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sitema2
+{
+    public partial class Form1 : Form
+    {
+        bool VerSenhaTxt = false;
+        private string usuarioCorreto = "admin";
+        private string senhaCorreta = "123456";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Adiciona o evento KeyDown ao TextBox de usuário e senha
+            textBoxUsuario.KeyDown += new KeyEventHandler(textBox_KeyDown);
+            textBoxSenha.KeyDown += new KeyEventHandler(textBox_KeyDown);
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Verifica se a tecla pressionada foi "Enter"
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Simula o clique do botão Entrar
+                buttonEntrar.PerformClick();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (VerSenhaTxt == false)
+            {
+                textBoxSenha.PasswordChar = '\0';
+                VerSenhaTxt = true;
+                buttonMostrarSenha.Text = "Ocultar Senha";
+            }
+            else
+            {
+                textBoxSenha.PasswordChar = '*';
+                VerSenhaTxt = false;
+                buttonMostrarSenha.Text = "Mostrar Senha";
+            }
+        }
+
+        private void buttonLimparCampos_Click(object sender, EventArgs e)
+        {
+            textBoxUsuario.Text = "";
+            textBoxSenha.Text = "";
+            textBoxUsuario.Focus();
+        }
+
+        private void buttonEntrar_Click(object sender, EventArgs e)
+        {
+            string usuario = textBoxUsuario.Text;
+            string senha = textBoxSenha.Text;
+
+            if (usuario == usuarioCorreto && senha == senhaCorreta)
+            {
+                labelMensagem.Text = "Login bem sucedido";
+                labelMensagem.ForeColor = Color.Green;
+                Menu form = new Menu();
+                form.ShowDialog();
+            }
+            else
+            {
+                labelMensagem.Text = "Usuário ou senha inválida";
+                labelMensagem.ForeColor = Color.Red;
+                textBoxUsuario.Text = "";
+                textBoxSenha.Text = "";
+                textBoxUsuario.Focus();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Sitema2/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sitema2
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           cadCliente form = new cadCliente();
+           form.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conCliente form = new conCliente();
+            form.ShowDialog();
+        }
+    }
+}
